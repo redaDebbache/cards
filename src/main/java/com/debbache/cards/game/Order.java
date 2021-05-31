f60d53bf87ad3bfa060e692e4a28356762aa4255
Reda <redad@ext-theodo.fr>
2021-05-31 08:19:04 +0200

    small fixes

--- src/main/java/com/debbache/cards/game/Order.java
+++ src/main/java/com/debbache/cards/game/Order.java
@@ -3,9 +3,7 @@ package com.debbache.cards.game;
 import com.debbache.cards.deck.Color;
 import lombok.Getter;
 
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,7 +12,14 @@ import static com.debbache.cards.utils.Constants.*;
 @Getter
 public class Order {
     private static final Random RANDOM = new Random();
-    private final List<Color> colorOrder = List.copyOf(Set.of(Color.values()));
+    private final List<Color> colorOrder = getshuffledColors();
+
+     List<Color> getshuffledColors() {
+        var colors = Arrays.asList(Color.values());
+        Collections.shuffle(colors);
+        return colors;
+    }
+
     private final List<Integer> valuesOrder = getRandomValuesWithRepetition();
 
     private static List<Integer> getRandomValuesWithRepetition() {
